7a1160cddf5729e67c811774b3c744febb562750
Leonardo <leonardomartinslopes2@gmail.com>
2025-11-18 23:44:00 -0300

    comentarios

--- prova4_20242343/kdtree.h
+++ prova4_20242343/kdtree.h
@@ -2,30 +2,31 @@
 #define KDTREE_H
 
 #include <stdio.h>
-#include <stdlib.h> // <-- LINHA ADICIONADA
+#include <stdlib.h>
 
-// Sem typedef, como solicitado
 struct Ponto{
     float *coords; 
     int classe;    
 };
 
-// Sem typedef, como solicitado
 struct Nodo {
-    struct Ponto *ponto; // Agora usa struct Ponto
+    struct Ponto *ponto;
     struct Nodo *esq;
     struct Nodo *dir;
 };
 
-// Protótipos agora usam "struct"
 struct Nodo* cria_arvore_entrada();
+
 struct Nodo* insere_nodo(struct Nodo* raiz, struct Ponto* p, int k);
+
 struct Nodo* busca_nodo(struct Nodo* raiz, float* alvo, int coord, int k);
+
 void imprime_largura(struct Nodo* raiz, int k);
+
 void z_vizinhos(struct Nodo* raiz, float* alvo, int z, int k);
+
 void libera_arvore(struct Nodo* raiz);
 
-// Prototipo do malloc_seguro para ser usado pelo main.c
-void* malloc_seguro(size_t tamanho); // <-- LINHA ADICIONADA
+void* malloc_seguro(size_t tamanho);
 
 #endif
